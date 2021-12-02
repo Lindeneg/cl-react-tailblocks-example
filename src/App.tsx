ae7213d4f8ced99dc73d1b47fd2a7319ccdde777
lindeneg <christian@lindeneg.org>
2021-12-02 03:54:59 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ThemeContext } from "cl-react-tailblocks";
+import type { ThemeContextContent } from "cl-react-tailblocks";
+import { Component } from "./Component";
 
 function App() {
+  const [cxt, setCxt] = useState<ThemeContextContent>({
+    theme: "dark",
+    color: "green",
+  });
+
+  const onSwitchTheme = () => {
+    setCxt((prev) => {
+      const light = prev.theme === "light";
+      return {
+        theme: light ? "dark" : "light",
+        color: light ? "green" : "indigo",
+      };
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider
+      value={{
+        theme: cxt.theme,
+        color: cxt.color,
+        setContext: onSwitchTheme,
+      }}
+    >
+      <div className="App">
+        <Component />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
